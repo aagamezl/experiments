718906225586b10403ddb6ab4196aace45aae375
Álvaro José Agámez Licha <45150637+aagamezl@users.noreply.github.com>
2025-01-17 08:39:01 +0000

    feat: shell script self install

---
+++ shell/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define the target path
+TARGET_PATH="/usr/local/bin/shest"
+GITHUB_URL="https://raw.githubusercontent.com/aagamezl/experiments/master/shell/install.sh"
+
+# Check if the script already exists
+if [ -f "$TARGET_PATH" ]; then
+  echo "shest is already installed at $TARGET_PATH."
+else
+  echo "Installing shest..."
+  # Download the script and save it to /usr/local/bin
+  curl -fsSL "$GITHUB_URL" -o "$TARGET_PATH"
+  
+  # Set execution permissions
+  chmod +x "$TARGET_PATH"
+  
+  if [ $? -eq 0 ]; then
+    echo "shest installed successfully at $TARGET_PATH."
+  else
+    echo "Failed to install shest. Please check your permissions."
+    exit 1
+  fi
+fi
